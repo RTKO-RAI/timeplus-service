99d1afcd64df8288e7d9da1cd7ae51968e75d534
ArdiJorganxhi <ardijorganxhiu@gmail.com>
2025-07-25 18:40:05 +0200

    initial commit

---
+++ src/main/java/com/rbinternational/neura/timeplus/TimeplusApplication.java
@@ -0,0 +1,13 @@
+package com.rbinternational.neura.timeplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeplusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbinternational/neura/timeplus/TimeplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbinternational.neura.timeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
