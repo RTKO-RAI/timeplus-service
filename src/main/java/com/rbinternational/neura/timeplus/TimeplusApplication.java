1e2fa46dfe0e5a172f3c4f0752ca5a49b86c4f75
Ahmet Bucko <ahmet.bucko@student.uni-pr.edu>
2025-08-09 13:48:26 +0200

    adding the mcp server

--- src/main/java/com/rbinternational/neura/timeplus/TimeplusApplication.java
+++ src/main/java/com/rbinternational/neura/timeplus/TimeplusApplication.java
@@ -1,7 +1,13 @@
 package com.rbinternational.neura.timeplus;
 
+import com.rbinternational.neura.timeplus.service.TimeRecordingService;
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class TimeplusApplication {
@@ -10,4 +16,8 @@ public class TimeplusApplication {
 		SpringApplication.run(TimeplusApplication.class, args);
 	}
 
+	@Bean
+	public List<ToolCallback> tools(TimeRecordingService timeRecordingService){
+		return List.of(ToolCallbacks.from(timeRecordingService));
+	}
 }
